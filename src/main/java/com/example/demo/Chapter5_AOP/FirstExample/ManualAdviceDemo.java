a335a3e013553198f9e1bf2fd7931a02a862bc52
Seba <sebastian.hinojosa.h@gmail.com>
2024-05-24 00:12:50 -0400

    Demo AOT - Pure Java - Additional comments. No new code added.

--- src/main/java/com/example/demo/Chapter5_AOP/FirstExample/ManualAdviceDemo.java
+++ src/main/java/com/example/demo/Chapter5_AOP/FirstExample/ManualAdviceDemo.java
@@ -5,21 +5,27 @@ import org.springframework.aop.framework.ProxyFactory;
 
 public class ManualAdviceDemo {
     public static void main(String[] args) {
+        // New object (original one) and execute the methods execute() and aftershow();
         Concert concert = new Concert();
         concert.execute();
         concert.afterShow();
 
-        LoggerFactory.getLogger(Concert.class).info("Newwww test");
+        LoggerFactory.getLogger(Concert.class).info("\nFrom now on the Proxy is working\n");
 
+        //New ProxyFactory
         ProxyFactory pf = new ProxyFactory();
+
+        //Add Advises
         pf.addAdvice(new SimpleBeforeAdvice());
         pf.addAdvice(new SimpleAroundAdvice());
         pf.addAdvice(new SimpleAfterAdvice());
+
+        //Set Target object where the Advises will apply
         pf.setTarget(concert);
 
+        //Get the proxy and execute the same methods.
         Performance proxy = (Performance) pf.getProxy();
         proxy.execute();
         proxy.afterShow();
-
     }
 }
